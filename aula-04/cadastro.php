51dbc61de1d1045000637f926cea8999761400c3
Professor Durvaldo <durvaldomarques2@gmail.com>
2026-08-12 14:03:36 -0300

    Add files via upload

---
+++ aula-04/cadastro.php
@@ -0,0 +1,31 @@
+<pre>
+<?php
+    // dados do banco de dados
+    $host       = 'localhost';
+    $dbName     = 'financas';
+    $dbUser     = 'root';
+    $dbPassword = '';
+
+    // conectando ao banco de dados
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$dbName;charset=utf8mb4",
+        $dbUser,
+        $dbPassword
+    );
+
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+    $nivel = $_POST['nivel'];
+    $data_nascimento = $_POST['data-nascimento'];
+
+    $query_sql = "INSERT INTO `usuarios` 
+                (`nome`, `email`, `senha`, `nivel`, `data-nascimento`) 
+                VALUES 
+                (?, ?, ?, ?, ?);
+    ";
+
+    $stmt = $pdo->prepare($query_sql);
+    $stmt->execute([$nome, $email, $senha, $nivel, $data_nascimento]);
+
+    header("location: index.php");
